dc1fd073394365cae7edf3fe5f81b69c645cf522
Kicchu02 <kicchu.2002@gmail.com>
2025-02-26 03:29:14 +0530

    Finalize setting up of postgresql and flyway migration

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.ktor)
+    id("org.flywaydb.flyway") version "9.22.0"
 }
 
 group = "com.example"
@@ -26,4 +27,14 @@ dependencies {
     implementation(libs.ktor.server.config.yaml)
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
+
+    implementation("org.postgresql:postgresql:42.7.2")
+}
+
+flyway {
+    url = "jdbc:postgresql://localhost:5432/demo_db"
+    user = "admin"
+    password = "Demo@123"
+    schemas = arrayOf("public")
+    locations = arrayOf("filesystem:src/main/resources/db/migration")
 }
