6ddf99c3bdad5ee40ca80d198280d2f1bb577104
Kicchu02 <kicchu.2002@gmail.com>
2025-08-13 18:18:54 +0530

    Restructure routes by module

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -11,57 +11,65 @@ import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
+import io.ktor.server.routing.route
 import io.ktor.server.routing.post
+import io.ktor.server.routing.Route
 import io.ktor.server.routing.routing
 
 fun Application.configureRouting() {
-    routing {
-        post("/signUp") {
-            try {
-                val signUp = inject<SignUp>()
-                val request = call.receive<SignUp.Request>()
-                val response = signUp.execute(request = request)
-                call.respond(response)
-            } catch (signUpException: SignUpException) {
-                when (signUpException) {
-                    is SignUpException.EmailAlreadyExistsException -> {
-                        call.response.status(value = HttpStatusCode.Conflict)
-                        call.respondText(text = "Email already exists.")
-                    }
+	routing {
+		route("/user") { userRoutes() }
+		route("/dummy") { dummyRoutes() }
+	}
+}
 
-                    is SignUpException.InsecurePasswordException -> {
-                        call.response.status(value = HttpStatusCode.BadRequest)
-                        call.respondText(text = "Password is insecure.")
-                    }
-                }
-            }
-        }
-        post("/signIn") {
-            try {
-                val signIn = inject<SignIn>(call)
-                val request = call.receive<SignIn.Request>()
-                val response = signIn.execute(request = request)
-                call.respond(message = response)
-            } catch (signInException: SignInException) {
-                when (signInException) {
-                    is SignInException.InvalidEmailId -> {
-                        call.response.status(value = HttpStatusCode.Unauthorized)
-                        call.respondText(text = "Email doesn't exist.")
-                    }
+private fun Route.userRoutes() {
+	post("/signUp") {
+		try {
+			val signUp = inject<SignUp>()
+			val request = call.receive<SignUp.Request>()
+			val response = signUp.execute(request = request)
+			call.respond(response)
+		} catch (signUpException: SignUpException) {
+			when (signUpException) {
+				is SignUpException.EmailAlreadyExistsException -> {
+					call.response.status(value = HttpStatusCode.Conflict)
+					call.respondText(text = "Email already exists.")
+				}
+				is SignUpException.InsecurePasswordException -> {
+					call.response.status(value = HttpStatusCode.BadRequest)
+					call.respondText(text = "Password is insecure.")
+				}
+			}
+		}
+	}
+	post("/signIn") {
+		try {
+			val signIn = inject<SignIn>(call)
+			val request = call.receive<SignIn.Request>()
+			val response = signIn.execute(request = request)
+			call.respond(message = response)
+		} catch (signInException: SignInException) {
+			when (signInException) {
+				is SignInException.InvalidEmailId -> {
+					call.response.status(value = HttpStatusCode.Unauthorized)
+					call.respondText(text = "Email doesn't exist.")
+				}
+				is SignInException.InvalidPassword -> {
+					call.response.status(value = HttpStatusCode.Unauthorized)
+					call.respondText(text = "Invalid password.")
+				}
+			}
+		}
+	}
+}
 
-                    is SignInException.InvalidPassword -> {
-                        call.response.status(value = HttpStatusCode.Unauthorized)
-                        call.respondText(text = "Invalid password.")
-                    }
-                }
-            }
-        }
-        // Authenticated dummy API example
-        get("/dummy") {
-            val response = call.executeAuthenticated<DummyApi, DummyApi.Request, DummyApi.Response>(
-                request = call.receive<DummyApi.Request>(),
-            )
-            call.respond(message = response)
-        }
-    }
+private fun Route.dummyRoutes() {
+	// Authenticated dummy API example
+	get("/dummy") {
+		val response = call.executeAuthenticated<DummyApi, DummyApi.Request, DummyApi.Response>(
+			request = call.receive<DummyApi.Request>(),
+		)
+		call.respond(message = response)
+	}
 }
